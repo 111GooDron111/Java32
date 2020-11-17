4bded430165e1592dc70a273ae4c6c47a48d7568
Dmitry Abramovskikh <dmabram15@gmail.com>
2020-11-17 22:44:57 +0300

    Initial commit

---
+++ java/Goods.java
@@ -0,0 +1,15 @@
+public class Goods {
+
+    private String name;
+
+    public int amount;
+
+    public Goods(String name, int amount) {
+        this.name = name;
+        this.amount = amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
